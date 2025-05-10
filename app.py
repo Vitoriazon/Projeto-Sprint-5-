2951ffbb1f940eb1f60fa2aefe856bc10e983561
Vitória <vitoriazon3004@gmail.com>
2025-05-10 14:48:11 -0300

    Nome do arquivo atualizado

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
+car_data = pd.read_csv('./vehicles_us.csv') # lendo os dados
 
 st.header('Dashboard de Veículos - Webinar Sprint 5')
 
